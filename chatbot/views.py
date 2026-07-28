9b47b5bd80a934f619d1f6d3f3edba13da741c05
mac <mac@192.168.1.7>
2026-07-28 16:33:27 +0100

    Ajout de l'API /api/chat/ (chatbot/views.py, chatbot/urls.py)

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
+from .services import repondre
+
+
+@csrf_exempt
+@require_POST
+def chat_api(request):
+    donnees = json.loads(request.body)
+    message = donnees.get("message", "")
+
+    resultat = repondre(message)
+
+    return JsonResponse(resultat)
